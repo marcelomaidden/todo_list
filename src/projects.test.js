3c6d4b1335b794d545fcfa10035023080184e2f1
Marcelo Fernandes de Araujo <marcelomaidden@gmail.com>
2021-01-19 17:59:19 -0300

    Check if default project exists on newly created object

--- src/projects.test.js
+++ src/projects.test.js
@@ -1,3 +1,12 @@
+import Projects from './projects'
+import Project from './project'
+let projects = new Projects();
 test('Create a Projects object', () => {
-  expect(new Projects).toBeInstanceOf(Projects);
+  expect(projects).toBeInstanceOf(Projects);
 })
+
+test('Newly created project owns default project', () => {
+  const expected = [new Project('default', 'hsl(204, 86%, 53%)')]
+  expect(projects.myProjects).toEqual(expect.arrayContaining(expected));
+})
+
