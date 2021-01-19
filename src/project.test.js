e3e29be050d6aae49173cadd974d7ea5ad218a47
Marcelo Fernandes de Araujo <marcelomaidden@gmail.com>
2021-01-19 17:44:46 -0300

    Add project integration tests

--- src/project.test.js
+++ src/project.test.js
@@ -1,5 +1,13 @@
 const { TestScheduler } = require("jest");
+import Project from './project';
+
 test('Create a project object', () => {
   let project = new Project('Tests in JavaScript', '#FFF');
-  expect(project).toBe(project);
-})
+  expect(project).toBeInstanceOf(Project);
+});
+
+test("Throw error if Project's name and color were provided", () => {
+  expect(() => {
+    new Project();
+  }).toThrow("Cannot read property 'split' of undefined");
+});
