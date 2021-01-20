f2bc8ede3d7a022f1b63e973766eccf2d4133338
Marcelo Fernandes de Araujo <marcelomaidden@gmail.com>
2021-01-19 22:16:12 -0300

    add listTodos test

--- src/todoList.test.js
+++ src/todoList.test.js
@@ -1,13 +1,31 @@
 import TodoList from './todoList'
+import Todo from './todo';
 
 describe('Todo list', () => {
   let todoList;
 
   beforeEach(() => {
-    todoList = new TodoList()
+    todoList = new TodoList();
   });
 
   it('Creates a todo list object', () => {
     expect(todoList).toBeInstanceOf(TodoList)
+  });
+
+  it('Add todo on myTodos array', () => {
+    let todo = new Todo('Jest', '#FFF');
+    todoList.addTodo(todo);
+    expect(todoList.myTodos).toEqual(expect.arrayContaining([todo]))
+  });
+
+  it('Remove todo from myTodos array', () => {
+    let todo = new Todo('Jest', '#FFF');
+    todoList.addTodo(todo);
+    todoList.remove(todo.id);
+    expect(todoList.myTodos).toEqual(expect.arrayContaining([]))
+  })
+
+  it('Test listTodos method', () => {
+    expect(todoList.listTodos()).toBe(undefined)
   })
 })
